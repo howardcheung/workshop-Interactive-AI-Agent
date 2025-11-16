e8a944f09448e99613cd39a5fffd4d382da4e6a4
howardcheung <125422287+cex-hcheung@users.noreply.github.com>
2025-11-16 14:00:19 +0800

    Example streamlit app.py

---
+++ Tutorial 3/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import numpy as np
+import time
+
+st.title("Plotting Demo")
+st.caption("""
+This demo illustrates a combination of plotting and animation with Streamlit. We're generating a bunch of random numbers in a loop for around 5 seconds. Enjoy!
+"""
+)
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)  # noqa: NPY002
+chart = st.line_chart(last_rows)
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)  # noqa: NPY002
+    status_text.text(f"{i}% complete")
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Rerun")
